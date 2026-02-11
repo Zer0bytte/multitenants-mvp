5284eed92c3054e03bf300a950e1e5e5e12c416d
Marko Medhat <85482481+Zer0bytte@users.noreply.github.com>
2026-02-11 17:28:33 +0200

    Scaffold tenants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_11_151557) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_11_152612) do
+  create_table "tenants", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
